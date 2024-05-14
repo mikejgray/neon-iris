mikejgray/neon-iris

commit 824a912d39d29d36e9b92fcb406db9724153fcf4

feat: browser sat (#51)
# Description
Introduces a web browser voice satellite option with OpenWakeWord and
Silero VAD running in JavaScript, all served via FastAPI. Allows for
both text and voice commands to a Neon Diana deployment.

This new option may one day replace the Gradio webpage.

---------

Co-authored-by: Daniel McKnight <daniel@neon.ai>

Dockerfile
@@ -1,21 +1,55 @@
+# Stage 1: Use a base image to install ffmpeg
+FROM jrottenberg/ffmpeg:4.1 as ffmpeg-base
+
+# Stage 2: Build the final image
 FROM python:3.8-slim
 
+# Label for vendor
 LABEL vendor=neon.ai \
     ai.neon.name="neon-iris"
 
-ENV OVOS_CONFIG_BASE_FOLDER neon
-ENV OVOS_CONFIG_FILENAME neon.yaml
-ENV XDG_CONFIG_HOME /config
+# Build argument for specifying extras
+ARG EXTRAS
 
-RUN apt update && \
-    apt install -y ffmpeg
+ENV OVOS_CONFIG_BASE_FOLDER=neon \
+    OVOS_CONFIG_FILENAME=neon.yaml \
+    XDG_CONFIG_HOME=/config
 
-ADD . /neon_iris
+# Copy ffmpeg binaries from the ffmpeg-base stage
+COPY --from=ffmpeg-base /usr/local/bin/ /usr/local/bin/
+COPY --from=ffmpeg-base /usr/local/lib/ /usr/local/lib/
+
+RUN mkdir -p /neon_iris/requirements
+COPY ./requirements/* /neon_iris/requirements
+
+RUN pip install wheel && pip install -r /neon_iris/requirements/requirements.txt
+RUN if [ "$EXTRAS" = "gradio" ]; then \
+        pip install -r /neon_iris/requirements/gradio.txt; \
+    elif [ "$EXTRAS" = "web_sat" ]; then \
+        pip install -r /neon_iris/requirements/web_sat.txt; \
+    else \
+        pip install -r /neon_iris/requirements/requirements.txt; \
+    fi
+
 WORKDIR /neon_iris
-
-RUN pip install wheel && \
-    pip install .[gradio]
+ADD . /neon_iris
+RUN pip install .
 
 COPY docker_overlay/ /
 
-CMD ["iris", "start-gradio"]
+# Expose port 8000 for websat
+EXPOSE 8000
+
+# Set the ARG value as an environment variable
+ENV EXTRAS=${EXTRAS}
+
+# Create a non-root user with a home directory and change ownership of necessary directories
+
+RUN groupadd -r neon && useradd -r -m -g neon neon \
+    && mkdir -p /config/neon \
+    && chown -R neon:neon /neon_iris /usr/local/bin /config
+
+# Use the non-root user to run the container
+USER neon
+
+ENTRYPOINT ["/neon_iris/entrypoint.sh"]

.dockerignore
@@ -1,0 +1,5 @@
+.git/
+tests/
+*.egg-info
+build/
+dist/

.github/workflows/publish_release.yml
@@ -10,7 +10,16 @@ jobs:
   build_and_publish_pypi_and_release:
     uses: neongeckocom/.github/.github/workflows/publish_stable_release.yml@master
     secrets: inherit
-  build_and_publish_docker:
+  build_and_publish_docker_gradio:
     needs: build_and_publish_pypi_and_release
     uses: neongeckocom/.github/.github/workflows/publish_docker.yml@master
     secrets: inherit
+    with:
+      build_args: EXTRAS=gradio
+    build_and_publish_docker_websat:
+      needs: build_and_publish_pypi_and_release
+      uses: neongeckocom/.github/.github/workflows/publish_docker.yml@master
+      secrets: inherit
+      with:
+        build_args: EXTRAS=web_sat
+        image_name: ${{ github.repository }}-websat

.github/workflows/publish_test_build.yml
@@ -16,7 +16,16 @@ jobs:
       version_file: "neon_iris/version.py"
       setup_py: "setup.py"
       publish_prerelease: true
-  build_and_publish_docker:
+  build_and_publish_docker_gradio:
     needs: publish_alpha_release
     uses: neongeckocom/.github/.github/workflows/publish_docker.yml@master
     secrets: inherit
+    with:
+      build_args: EXTRAS=gradio
+  build_and_publish_docker_websat:
+    needs: publish_alpha_release
+    uses: neongeckocom/.github/.github/workflows/publish_docker.yml@master
+    secrets: inherit
+    with:
+      build_args: EXTRAS=web_sat
+      image_name: ${{ github.repository }}-websat

docker_overlay/etc/neon/neon.yaml
@@ -12,6 +12,7 @@ iris:
   webui_chatbot_label: Chat History
   webui_mic_label: Speak to Neon
   webui_text_label: Text with Neon
+  webui_ws_url: ws://localhost:8000/ws # Override, as this needs to be reachable by the browser
   server_address: "0.0.0.0"
   server_port: 7860
   default_lang: en-us

docker_overlay/neon_iris/entrypoint.sh
@@ -1,10 +1,8 @@
-# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+#!/bin/bash
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
 # All trademark and other rights reserved by their respective owners
 # Copyright 2008-2024 Neongecko.com Inc.
-# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
-# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
-# Mike Gray, David Scripka
-# BSD-3 License
+# BSD-3
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 # 1. Redistributions of source code must retain the above copyright notice,
@@ -27,4 +25,13 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = "0.1.0"
+set -e
+
+if [ "$EXTRAS" = "gradio" ]; then
+    exec iris start-gradio
+    elif [ "$EXTRAS" = "web_sat" ]; then
+    exec iris start-websat
+else
+    echo "No extras specified, showing help. To execute a command, use 'docker run iris <command>'"
+    exec iris -h
+fi

neon_iris/cli.py
@@ -138,6 +138,19 @@ def start_gradio():
         click.echo("Unable to connect to MQ server")
 
 
+@neon_iris_cli.command(help="Create a Web Voice Satellite session")
+@click.option("--port", "-p", default=8000, help="Port to run on, defaults to 8000")
+@click.option("--host", default="0.0.0.0", help="Host to run on, defaults to 0.0.0.0")
+def start_websat(port, host):
+    from neon_iris.web_sat_client import app
+    _print_config()
+    try:
+        import uvicorn
+        uvicorn.run(app, host=host, port=port)
+    except OSError:
+        click.echo("Unable to connect to MQ server")
+
+
 @neon_iris_cli.command(help="Query Neon Core for supported languages")
 def get_languages():
     from neon_iris.util import query_neon

neon_iris/client.py
@@ -333,7 +333,7 @@ class NeonAIClient:
         self._send_serialized_message(serialized)
 
     def _send_audio(self, audio_file: str, lang: str,
-                    username: str, user_profiles: list,
+                    username: Optional[str], user_profiles: Optional[list],
                     context: Optional[dict] = None):
         context = context or dict()
         audio_data = encode_file_to_base64_string(audio_file)

commit 8060a93ae85e99f857513d5823f2fb432a69928d

fix: appropriate dependencies (#61)
# Description
Fixes the gradio audio input bug introduced in #51 

# Issues
Closes #59 
Replaces #60 

# Other Notes
Breaks caching quite a bit, so if that's an issue we can create a helper
image that we use to pull in dependencies.

Dockerfile
@@ -1,7 +1,3 @@
-# Stage 1: Use a base image to install ffmpeg
-FROM jrottenberg/ffmpeg:4.1 as ffmpeg-base
-
-# Stage 2: Build the final image
 FROM python:3.8-slim
 
 # Label for vendor
@@ -14,10 +10,8 @@ ARG EXTRAS
 ENV OVOS_CONFIG_BASE_FOLDER=neon \
     OVOS_CONFIG_FILENAME=neon.yaml \
     XDG_CONFIG_HOME=/config
-
-# Copy ffmpeg binaries from the ffmpeg-base stage
-COPY --from=ffmpeg-base /usr/local/bin/ /usr/local/bin/
-COPY --from=ffmpeg-base /usr/local/lib/ /usr/local/lib/
+# Set the ARG value as an environment variable
+ENV EXTRAS=${EXTRAS}
 
 RUN mkdir -p /neon_iris/requirements
 COPY ./requirements/* /neon_iris/requirements
@@ -37,11 +31,11 @@ RUN pip install .
 
 COPY docker_overlay/ /
 
-# Expose port 8000 for websat
-EXPOSE 8000
-
-# Set the ARG value as an environment variable
-ENV EXTRAS=${EXTRAS}
+RUN apt-get update \
+  && apt-get install -y libsndfile1 libasound2 ffmpeg \
+  && apt-get --purge autoremove -y \
+  && apt-get clean \
+  && rm -rf "${HOME}"/.cache /var/lib/apt /var/log/{apt,dpkg.log}
 
 # Create a non-root user with a home directory and change ownership of necessary directories
 